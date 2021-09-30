5d4c73d68302c371ea9e6ccca78923ad0c5eaae5
kbalev <kamen_balev@yahoo.com>
2021-09-30 14:53:23 +0100

    added the base java springboot directory

---
+++ src/main/java/com/softwareinstitute/training/keiran/springbootForays/springbotForays/SpringbotForaysApplication.java
@@ -0,0 +1,13 @@
+package com.softwareinstitute.training.keiran.springbootForays.springbotForays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbotForaysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbotForaysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softwareinstitute/training/keiran/springbootForays/springbotForays/SpringbotForaysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softwareinstitute.training.keiran.springbootForays.springbotForays;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbotForaysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
