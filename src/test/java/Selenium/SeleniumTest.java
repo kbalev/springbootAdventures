f260b4d4306db06afdee2d5451bfea22602603f4
kbalev <kamen_balev@yahoo.com>
2021-10-19 11:59:18 +0100

    added a comment

--- src/test/java/Selenium/SeleniumTest.java
+++ src/test/java/Selenium/SeleniumTest.java
@@ -10,7 +10,7 @@ public class SeleniumTest {
     public static void main(String[] args) {
         //make sure to have geckodriver at the relevant filepath
         System.setProperty("webdriver.gecko.driver","D:\\CodeStuffs\\geckodriver-v0.30.0-win64\\geckodriver.exe");
-
+        // need to write a test for my own app
         WebDriver driver = new FirefoxDriver();
         driver.get("https://www.google.com");
         driver.manage().window().maximize();
